47cdacedbc0a174d4026db7f9ae9f3f4e99a1ea1
Com1640 <trungnbgcs17560@fpt.edu.vn>
2021-11-14 17:01:37 +0700

    commit contact

--- resources/views/pages/contact_us/contact_us.blade.php
+++ resources/views/pages/contact_us/contact_us.blade.php
@@ -31,18 +31,18 @@
 	    			<div class="contact-info">
 	    				<h2 class="title text-center">Contact Info</h2>
 	    				<address>
-	    					<p>E-Shopper Inc.</p>
-							<p>935 W. Webster Ave New Streets Chicago, IL 60614, NY</p>
-							<p>Newyork USA</p>
-							<p>Mobile: +2346 17 38 93</p>
-							<p>Fax: 1-714-252-0026</p>
-							<p>Email: info@e-shopper.com</p>
+	    					<p>ChrisBo</p>
+							<p>Greenwich University</p>
+							<p>United Kingdom</p>
+							<p>Mobile: +84 369631514</p>
+							<p>Fax: no fax</p>
+							<p>Email: Trungnb301@gmail.com</p>
 	    				</address>
 	    				<div class="social-networks">
 	    					<h2 class="title text-center">Social Networking</h2>
 							<ul>
 								<li>
-									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="https://www.facebook.com/baotrungas99/"><i class="fa fa-facebook"></i></a>
 								</li>
 								<li>
 									<a href="#"><i class="fa fa-twitter"></i></a>
